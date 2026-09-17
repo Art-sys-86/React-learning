79287e25c10284960f2fadf6fee5a4eb7de06d3c
Art <hithukten@gmail.com>
2026-09-18 00:31:18 +0600

    Learning Components!

---
+++ my-first-react-app/src/greeting.jsx
@@ -0,0 +1,13 @@
+function Greeting(){
+    return (
+    <>
+    <h1>"Hello I'm a Component"</h1>
+    <h2>"And I love to eat momo!</h2>
+    <h3>"Do you love momo also?"</h3>
+    <p>PS: MOMO is pretty popular in the Asia countries</p>
+    </>
+    )
+};
+
+export default Greeting;
+
