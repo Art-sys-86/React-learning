3beeb833b8b7d4ecead2b2157adca8d5cffeb317
Art <hithukten@gmail.com>
2026-09-18 18:59:22 +0600

    Learning JSX

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Greeting from './greeting.jsx'
+import App from './convert.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <App />
   </StrictMode>,
 )
